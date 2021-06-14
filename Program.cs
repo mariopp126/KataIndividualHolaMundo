7713eb3e96a4cda984fa90e2c9c0c10b5dc2df66
Mario <mpucheu126>
2021-06-14 17:11:33 -0400

    commit programa corriendo correctamente

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace kataIndividualHolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var holaMundo = "Hola Mundo";
+            var noSpaces = Regex.Replace(holaMundo, @"\s", "");
+            Console.WriteLine("{0}", noSpaces);
+        }
+    }
+}
